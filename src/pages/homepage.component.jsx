39a9b316439d1cc2648cdbfcbe8434e2c33dea33
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-27 23:00:46 -0600

    form the basic of the home page.

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,45 +1,12 @@
 import React from "react";
 
+import Directory from "../components/directory/directory.component";
+
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Hats</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Jackets</h1>
-          <span className="subitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Sneakers</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Womens</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">Mens</h1>
-          <span className="subtitle">Shop Now</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
